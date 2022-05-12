a55c7f517cac66853d0751487214448b1b3a1776
Sendreto9116 <c.sendreto9116@gmail.com>
2022-05-11 21:01:16 -0300

    primeira

---
+++ Java/src/App.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primeiro) throws Exception {
+        primeiro.setTitle("Locadora");
+        
+    }
+}
